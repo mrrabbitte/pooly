af2e5a08bbfc0b82ee937dfee78b6fce955f3e8d
P. Gli <pgli@pm.me>
2022-07-09 22:34:05 +0100

    Added mt tests for the leaky bucket + minor rename

--- src/services/limits/mod.rs
+++ src/services/limits/mod.rs
@@ -12,7 +12,7 @@ struct LeakyBucket<T> where T: NowProvider {
     clock: T,
 
     tickets: AtomicU32,
-    threshold: u32,
+    max_requests_per_period: u32,
 
     period_millis: u128,
     last_updated_at_millis: Mutex<u128>
@@ -22,14 +22,14 @@ struct LeakyBucket<T> where T: NowProvider {
 impl<T: NowProvider>  LeakyBucket<T> {
 
     pub fn new(clock: T,
-               threshold: u32,
+               max_requests_per_period: u32,
                period_millis: u128) -> LeakyBucket<T> {
         let now_millis = clock.now_millis();
 
         LeakyBucket {
             clock,
             tickets: AtomicU32::new(0),
-            threshold,
+            max_requests_per_period,
             period_millis,
             last_updated_at_millis: Mutex::new(now_millis)
         }
@@ -47,13 +47,13 @@ impl<T: NowProvider>  LeakyBucket<T> {
         }
 
         return Err(LeakyBucketError::TooManyRequests {
-            threshold: self.threshold,
+            threshold: self.max_requests_per_period,
             period_millis: self.period_millis
         });
     }
 
     fn has_free_tickets(&self) -> bool {
-        self.tickets.fetch_add(1, Ordering::SeqCst) < self.threshold
+        self.tickets.fetch_add(1, Ordering::SeqCst) < self.max_requests_per_period
     }
 
     fn update_tickets(&self) -> Result<(), LeakyBucketError> {
@@ -86,19 +86,22 @@ pub enum LeakyBucketError {
 #[cfg(test)]
 mod tests {
     use std::sync::Arc;
-    use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
+    use std::sync::atomic::{AtomicU32, AtomicU64, AtomicUsize, Ordering};
+    use std::thread;
     use std::time::Duration;
 
     use crate::models::utils::time::{Clock, now_millis, NowProvider};
     use crate::services::limits::LeakyBucket;
 
     const PERIOD_MILLIS: u128 = 10_000; // assuming the test will run in < 10 sec
-    const THRESHOLD: u32 = 3;
+    const MAX_REQUESTS_PER_PERIOD: u32 = 3;
+
+    const NUM_NANOS_IN_MICRO: u32 = 1_000;
 
     #[test]
     fn test_creates_bucket_correctly() {
         let bucket =
-            LeakyBucket::new(Clock::new(), THRESHOLD, PERIOD_MILLIS);
+            LeakyBucket::new(Clock::new(), MAX_REQUESTS_PER_PERIOD, PERIOD_MILLIS);
 
         assert!(bucket.acquire().is_ok());
     }
@@ -106,7 +109,7 @@ mod tests {
     #[test]
     fn test_disallows_for_abuse_of_quota() {
         let bucket =
-            LeakyBucket::new(Clock::new(), THRESHOLD, PERIOD_MILLIS);
+            LeakyBucket::new(Clock::new(), MAX_REQUESTS_PER_PERIOD, PERIOD_MILLIS);
 
         assert!(bucket.acquire().is_ok());
         assert!(bucket.acquire().is_ok());
@@ -122,7 +125,7 @@ mod tests {
 
         let bucket =
             LeakyBucket::new(
-                MockClock { now: now.clone() }, THRESHOLD, PERIOD_MILLIS);
+                MockClock { now: now.clone() }, MAX_REQUESTS_PER_PERIOD, PERIOD_MILLIS);
 
         assert!(bucket.acquire().is_ok());
         assert!(bucket.acquire().is_ok());
@@ -137,6 +140,44 @@ mod tests {
         assert!(bucket.acquire().is_ok());
 
         assert!(bucket.acquire().is_err());
+    }
+
+    #[test]
+    fn test_multiple_threads_holds_threshold() {
+        let num_successes = Arc::new(AtomicU32::new(0));
+        let now = Arc::new(AtomicU64::new(0));
+
+        let bucket = Arc::new(LeakyBucket::new(
+            MockClock { now: now.clone() }, MAX_REQUESTS_PER_PERIOD, PERIOD_MILLIS));
+
+        spawn_worker(bucket.clone(), num_successes.clone());
+        spawn_worker(bucket.clone(), num_successes.clone());
+        spawn_worker(bucket.clone(), num_successes.clone());
+
+        thread::sleep(Duration::from_millis(1));
+
+        assert_eq!(num_successes.load(Ordering::SeqCst), MAX_REQUESTS_PER_PERIOD);
+
+        now.fetch_add(PERIOD_MILLIS as u64, Ordering::SeqCst);
+
+        thread::sleep(Duration::from_millis(1));
+
+        assert_eq!(num_successes.load(Ordering::SeqCst), MAX_REQUESTS_PER_PERIOD * 2);
+
+        thread::sleep(Duration::from_millis(10));
+
+        assert_eq!(num_successes.load(Ordering::SeqCst), MAX_REQUESTS_PER_PERIOD * 2);
+    }
+
+    fn spawn_worker(bucket: Arc<LeakyBucket<MockClock>>,
+                    num_successes: Arc<AtomicU32>) {
+        thread::spawn(move ||
+            loop {
+                if bucket.acquire().is_ok() {
+                    num_successes.fetch_add(1, Ordering::SeqCst);
+                    thread::sleep(Duration::new(0, NUM_NANOS_IN_MICRO));
+                }
+            });
     }
 
     struct MockClock {
